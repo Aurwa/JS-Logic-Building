40ad8ae56e752a9749310589d9161d14748d5442
Aurwa <zafaraurwa11@gmail.com>
2024-09-04 11:18:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 document.write("Hello World");
 
 let sqrt = Math.sqrt(64);
-console.log(sqrt);
+// console.log(sqrt);
+
+// calculating area of a triangle
+/*
+let sideOne = Number(prompt("Enter side one: "));
+let sideTwo = Number(prompt("Enter side two: "));
+let sideThree = Number(prompt("Enter side three: "));
+let s = (sideOne + sideTwo + sideThree) / 2;
+console.log("s: ", s);
+
+let area = Math.sqrt(s * (s - sideOne) * (s - sideTwo) * (s - sideThree));
+console.log(area);
+*/
+
+// Swapping numbers using temporary variable
+let temp;
+let a = 5;
+let b = 6;
+temp = a;
+a = b;
+b = temp;
+console.log("a: ", a);
+console.log("b: ", b);
